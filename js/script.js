443553e4ef029ff6e99aff72e95bf91011865a61
bittoMazza <robertomazza99@gmail.com>
2022-06-14 15:50:17 +0200

    add console.warn

--- js/script.js
+++ js/script.js
@@ -10,12 +10,14 @@ ticketPrice = userNumKm * 0.21;
 if (isNaN(userNumKm) == true || userNumKm < 5) 
 {
   alert('ATTENZIONE : NUMERO KM NON VALIDO, RICARICARE LA PAGINA');
+  console.warn('ATTENZIONE : NUMERO KM NON VALIDO, RICARICARE LA PAGINA');
 } 
 //Ipotizziamo che non esistano persone con piu di 120 anni
 else if(isNaN(userAge) == true || (userAge < 0 || userAge > 120))
 {
     alert('ATTENZIONE : ETA\' NON VALIDA, RICARICARE LA PAGINA');
-}
+    console.warn('ATTENZIONE : ETA\' NON VALIDA, RICARICARE LA PAGINA');
+} 
 else{
     if(userAge < 18)
     {
@@ -31,6 +33,7 @@ else{
         console.log('Il prezzo del biglietto è : €'+ saleTicketPrice)
     }
     else{
+        ticketPrice = ticketPrice.toFixed(2);
         document.getElementById('ticket-price').innerHTML = 'Il prezzo del biglietto è : €'+ ticketPrice;
         console.log('Il prezzo del biglietto è : €'+ ticketPrice)
     }
